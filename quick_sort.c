2a91ccaa74b8ff7f78f7bf0409f9ad01f2e2e8c0
Matthew Williams <mwilliams@HVTL0158.(none)>
2012-01-30 14:03:03 +0800

    works using 2 array

--- quick_sort.c
+++ quick_sort.c
@@ -2,14 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 
-void quicksort(int a[], int size) {
+void quicksort(int a[], int left_pos, int right_pos);
+int partition(int a[], int left_pos, int right_pos);
+
+void quicksort(int a[], int left_pos, int right_pos) {
+  if (left_pos < right_pos) {
+    int pi = partition(a, left_pos, right_pos);
+    quicksort(a, left_pos, pi - 1);
+    quicksort(a, pi + 1, right_pos);
+  }
+}
+
+int partition(int a[], int left_pos, int right_pos) {
+  int store, i;
+  int pivot = (right_pos + left_pos) / 2;
+  swap(&a[pivot], &a[right_pos]);
+  store = left_pos;
+  for (i = left_pos; i < right_pos; i++) {
+    if (a[i] <= a[right_pos]) {
+      swap(&a[i], &a[store]);
+      store++;
+    }
+  }
+  swap(&a[store], &a[right_pos]);
+  return store;
 }
 
 test_empty_list() {
   int list_to_sort[0] = {};
   int expected[0] = {};
 
-  quicksort(list_to_sort, 0);
+  quicksort(list_to_sort, 0, 0);
 
   assert(memcmp(list_to_sort, expected, sizeof(int)*0) == 0);
 }
@@ -18,7 +41,7 @@ test_unsorted_list_size_one() {
   int list_to_sort[1] = {1};
   int expected[1] = {1};
 
-  quicksort(list_to_sort, 1);
+  quicksort(list_to_sort, 0, 0);
 
   print_array(list_to_sort, 1);
 
@@ -29,7 +52,7 @@ test_unsorted_list_size_two() {
   int list_to_sort[2] = {2, 1};
   int expected[2] = {1, 2};
 
-  quicksort(list_to_sort, 2);
+  quicksort(list_to_sort, 0, 1);
 
   print_array(list_to_sort, 2);
 
