09b2e006b512e551014a3190fe9b073c1b657ce5
Matthew Williams <mwilliams@HVTL0158.(none)>
2012-02-02 19:26:08 +0000

    pass length as arg

--- binary_search.c
+++ binary_search.c
@@ -1,8 +1,9 @@
 #include <assert.h>
+#include <stdio.h>
 
-int binary_search(int a[], int term) {
+int binary_search(int a[], int length, int term) {
   int low = 0;
-  int high = sizeof(a)/sizeof(int);
+  int high = length - 1;
   while (low <= high) {
     int ix = (low + high)/2;
     if (term == a[ix]) {
@@ -22,19 +23,28 @@ void test_empty_list() {
   int list[0] = {};
   int term = 5;
 
-  assert(binary_search(list, term) == 0);
+  assert(binary_search(list, 0, term) == 0);
 }
 
 void test_not_in_list() {
   int list[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int term = 11;
 
-  assert(binary_search(list, term) == 0);
+  assert(binary_search(list, 10, term) == 0);
 }
+
+void test_in_list() {
+  int list[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int term = 5;
+
+  assert(binary_search(list, 10, term) == 1);
+}
+
 
 
 
 int main() {
   test_empty_list();
   test_not_in_list();
+  test_in_list();
 }
